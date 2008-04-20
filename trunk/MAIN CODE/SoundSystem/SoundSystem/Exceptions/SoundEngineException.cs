de92e32f97cd41dafea492b8df26ca890af853f2
w_kucharzak@o2.pl <w_kucharzak@o2.pl@cad45fe6-8547-0410-bcc6-e12502920475>
2008-04-20 20:09:57 +0000

    klasa wyjatku

---
+++ trunk/MAIN CODE/SoundSystem/SoundSystem/Exceptions/SoundEngineException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.DirectX;
+
+namespace SoundSystem.Exceptions
+{
+    class SoundEngineException : DirectXException
+    {
+        private String publicMessage;
+
+        public String PublicMessage
+        {
+            get { return publicMessage; }
+            set { publicMessage = value; }
+        }
+
+        public SoundEngineException(String msg)
+        {
+            //mes
+        }
+	
+    }
+}
